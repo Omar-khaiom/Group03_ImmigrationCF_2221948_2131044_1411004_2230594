6db460bdd3b474709b8318d3c0db9578058e9cda
Omar-khaiom <2230594@iub.edu.bd>
2024-07-16 02:26:08 +0600

    Initial commit

---
+++ finall_project1/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.oop.finall_project1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.oop.finall_project1 to javafx.fxml;
+    exports com.oop.finall_project1;
+}
